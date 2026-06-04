ccd8517c2edf8f6b7aa8b41cd9307cd68390df0f
Choi Min-seo <azir8655@gmail.com>
2026-06-04 13:33:08 +0900

    [level 0] Title: 문자열 돌리기, Time: 11.05 ms, Memory: 15.7 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+       String str = Console.ReadLine();
+       foreach(char c in str){
+           Console.WriteLine(c);
+       }
+
+    }
+}
