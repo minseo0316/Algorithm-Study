80acf6ebb81b55509c77652a1c1a3962e06c14d2
Choi Min-seo <azir8655@gmail.com>
2026-06-04 13:28:57 +0900

    [level 0] Title: 문자열 출력하기, Time: 21.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string[] str  = Console.ReadLine().Split(' ');
+        
+        string a = str[0];
+        
+        Console.WriteLine($"{a}");
+
+    }
+}
