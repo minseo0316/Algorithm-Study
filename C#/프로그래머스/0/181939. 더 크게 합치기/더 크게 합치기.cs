79bd17189914847e550626351e4a93c79aa733ba
Choi Min-seo <azir8655@gmail.com>
2026-06-04 13:34:34 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.77 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b) {
+        
+        int ab = int.Parse($"{a}{b}");
+        int ba = int.Parse($"{b}{a}");
+        return Math.Max(ab,ba);    }
+}
