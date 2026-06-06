9ab4f562b81ece95615a95478f546e3995b1116b
Choi Min-seo <azir8655@gmail.com>
2026-06-06 15:38:08 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.15 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for(int i= idx; i < arr.Length; i++){
+            if(arr[i] == 1){
+              return i;
+            }
+        }
+        return -1;
+        return answer;
+    }
+}
